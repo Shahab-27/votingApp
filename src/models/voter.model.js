90a5c7cc6ccb9579d9a32112b7155720dd389369
shahab_27 <shahabfardeen2019@gmail.com>
2025-08-20 17:58:20 +0530

    feat: add bcrypt for password hashing and implement pre-save hook in voter model

--- src/models/voter.model.js
+++ src/models/voter.model.js
@@ -1,39 +1,62 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
 
 const voterSchema = new Schema(
     {
-        username :{
-            type : String,
-            required : true,
-            unique : true,
+        username: {
+            type: String,
+            required: true,
+            unique: true,
         },
-        password : {
-            type : String,
-            required : true,
+        password: {
+            type: String,
+            required: true,
 
         },
-        aadhaarNo : {
-            type : Number,
-            required : true,
-            unique : true
+        aadhaarNo: {
+            type: Number,
+            required: true,
+            unique: true
         },
-        isAdmin : {
-            type : Boolean,
-            enum : ['voter','admin'],
-            default : 'voter'
+        isAdmin: {
+            type: String,
+            enum: ['voter', 'admin'],
+            default: 'voter'
         },
-        isVoted : {
-            type : Boolean,
-            default : false
+        isVoted: {
+            type: Boolean,
+            default: false
         }
     }
 
-,
+    ,
 
-{
-    timestamps: true
-}
+    {
+        timestamps: true
+    }
 )
+// we dont use arrow f(x) because they dont have refrence of "this" object
+voterSchema.pre('save', async function (next) {
+    // hash the password if modified or its new
+    const person = this
+    if (!person.isModified('password')) return next()
+
+    try {
+        const hashedPassword = await bcrypt.hash(person.password, 10);
+        person.password = hashedPassword;
+        next();
+    } catch (error) {
+        return next(error);
+    }
+})
+
+voterSchema.methods.isPasswordCorrect = async function (userPassword) {
+    try {
+        return await bcrypt.compare(userPassword, this.password);
+    } catch (error) {
+        throw error;
+    }
+}
 
 
 const voter = mongoose.model("voter", voterSchema);
